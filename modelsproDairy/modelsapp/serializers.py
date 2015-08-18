6f041c8a32a1f2fcd48f41fc83020d485ff51368
Janardhana Adatravu <surya.janardhan@gmail.com>
2015-08-18 17:10:56 +0530

    Serializer class doc string updated

--- modelsproDairy/modelsapp/serializers.py
+++ modelsproDairy/modelsapp/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 from modelsapp.models import Contact
 
 class RestAppSerializer(serializers.ModelSerializer):
+      """
+         This is a serializer class for contact web api
+      """
       class Meta:
            model = Contact
            fields = ('first_name','last_name','sur_name','email','phone')
